cdfbb5af90823feef122031dba4f705ef0b7e50d
yanggz0606-fis <yanggz0606@gmail.com>
2026-04-07 17:49:04 +0800

    Update Cubox API and RSS feed list
    Updated the Cubox API endpoint and added new RSS feeds for various news sources.

--- rss2cubox/config.py
+++ rss2cubox/config.py
@@ -1,13 +1,31 @@
 # Cubox API
-CUBOX_API = 'https://cubox.pro/c/api/save/aabbcc'
+CUBOX_API = 'https://cubox.pro/c/api/save/aoJiW0tTy7d'
 # 需要添加的标签
 CUBOX_TAGS = None
 # 指定收藏夹
 CUBOX_FOLDER = "RSS"
 
 # 订阅源地址
-FEED_LIST = [
-    'https://www.fujirumors.com/feed/'
+# 配置你的 RSS 订阅源
+RSS_LIST = [
+    # 经济学人 - 商业版块 (逻辑严密，积累写作论点)
+    "https://www.economist.com/business/rss.xml",
+    # 科学美国人 - 全球资讯 (托福雅思学术阅读语感)
+    "http://rss.sciam.com/sciam/global-feed",
+    # BBC News - 世界新闻 (英式英语典范，积累时事词汇)
+    "http://feeds.bbci.co.uk/news/world/rss.xml",
+    # BBC News - 商业新闻 (雅思小作文背景知识)
+    "http://feeds.bbci.co.uk/news/business/rss.xml",
+    # 纽约时报 - 首页 (美式新闻标杆，风格多样)
+    "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+    # 路透社 - 最新新闻 (语言简洁客观，适合速读)
+    "http://feeds.reuters.com/reuters/topNews",
+    # 卫报 - 国际版 (英式深度报道)
+    "https://www.theguardian.com/world/rss",
+    # 自然 - 最新科研资讯 (拓展学术词汇)
+    "https://www.nature.com/nature.rss",
+    # 国家地理 - 每日新闻 (人文地理类阅读素材)
+    "feed://feeds.nationalgeographic.com/ng/News/News_Main",
 ]
 
 # 第一次运行时发送的最早的文章日期
